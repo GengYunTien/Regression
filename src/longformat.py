522cc6ea9eaff5a085cdbab5ddd168ba7b19f10a
GengYunTien <129279600+GengYunTien@users.noreply.github.com>
2024-09-08 14:52:42 +0800

    Update longformat.py

--- src/longformat.py
+++ src/longformat.py
@@ -9,6 +9,6 @@ rv = [f'{r}{v}' for r in R for v in V]
 dfAll = pd.DataFrame()
 for i in rv:
     df = pd.read_csv(os.path.join(root, '2024-pre-train', i[:-1], i[-1:] + '.csv'))
-    df = df.assign(odjID = i[:-1], condID = i[-1:])
+    df = df.assign(objID = i[:-1], condID = i[-1:])
     dfAll = pd.concat([dfAll, df], axis = 0, ignore_index = True)
 dfAll.to_csv(os.path.join(root, '2024-training.csv'), index = False)
